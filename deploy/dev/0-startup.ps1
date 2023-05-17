f948adc826b192f67ed14d389b803dbc49d4fbc0
f29 javi <javier.logrono@outlook.com>
2023-05-17 11:18:03 +0200

    delete file example

--- deploy/dev/0-startup.ps1
+++ deploy/dev/0-startup.ps1
@@ -2,15 +2,15 @@
 az account set --subscription 53348303-e009-4241-9ac7-a8e4465ece27
 az aks get-credentials --resource-group raitogpt2 --name fileLoaderDev
 
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.yaml
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.2/cert-manager.crds.yaml
+#kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.yaml
+#kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.2/cert-manager.crds.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.0/deploy/static/provider/cloud/deploy.yaml
 
-kubectl apply -f ./cert.yaml
-kubectl apply -f ./issuer.yaml
-kubectl apply -f ./fileuploader-ingress.yaml
-kubectl apply -f ./secrets-dev.yaml
-kubectl apply -f fileuploader-deployment.yaml
-kubectl apply -f fileuploader-service.yaml
+#kubectl apply -f ./updatedcertificate.yaml
+#kubectl apply -f ./fileuploader-ingress.yaml
+#kubectl apply -f ./secrets-dev.yaml
+#kubectl apply -f ./fileuploader-deployment.yaml
+#kubectl apply -f ./fileuploader-service.yaml
 
 # Show cluster
 kubectl cluster-info
